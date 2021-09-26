4b88f88053c55d984cefaf8ec4af0d4d70ad39cb
haruyuki@acrylic-figure <haruyuki.program@gmail.com>
2021-09-27 00:10:31 +0900

    ボタン操作で前の曲/次の曲に行けるように

--- rpi/application.py
+++ rpi/application.py
@@ -13,6 +13,12 @@ CENTER_BTN = 20
 RIGHT_BTN = 16
 CHIME_FILE = '/home/pi/acrylic-figure-display/rpi/chime.wav'
 
+# 変数宣言
+unit_number = 1
+music_number = 0
+loop_enable = False
+musics: dict
+
 # 初期化
 ### GPIO設定
 GPIO.setmode(GPIO.BCM)
@@ -52,15 +58,46 @@ def remove_num_and_ext(string: str) -> str:
     return string.split(' ')[1].split('.')[0]
 
 ### simpleaudioでファイルを再生する
-def play_file(file_path: str = ''):
+def play_file(file_path: str = '') -> sa.PlayObject:
     wav_obj = sa.WaveObject.from_wave_file(file_path)
     play_obj = wav_obj.play()
     return play_obj
 
+### ユニット番号と曲番号をグローバルから取得して再生する
+def play_music() -> sa.PlayObject:
+    file_name = musics[list(musics.keys())[unit_number]][music_number]
+    file_path = '/home/pi/musics/'+list(musics.keys())[unit_number]+'/'+file_name
+    send_string(remove_num_and_ext(file_name))
+    return play_file(file_path)
+
+### 曲番号を次に進める
+def next_music():
+    global music_number
+    global unit_number
+    if music_number + 1 < len(musics[list(musics.keys())[unit_number]]):
+        music_number += 1
+    elif unit_number + 1 < len(list(musics.keys())):
+        unit_number += 1
+        music_number = 0
+    else:
+        unit_number = 0
+        music_number = 0
+
+### 曲番号を前に戻す
+def prev_music():
+    global music_number
+    global unit_number
+    if not music_number - 1 < 0:
+        music_number = music_number - 1
+    elif not unit_number - 1 < 0:
+        unit_number = unit_number - 1
+        music_number = len(musics[list(musics.keys())[unit_number]]) - 1
+    else:
+        unit_number = len(musics.keys()) - 1
+        music_number = len(musics[list(musics.keys())[unit_number]]) - 1
+
 # メインプログラム
 try:
-    unit_number = 1
-    music_number = 0
     now_playing = play_file(CHIME_FILE)
     reset_arduino()
     musics = get_musics_list()
@@ -70,32 +107,32 @@ try:
         if not GPIO.input(LEFT_BTN):
             while(not GPIO.input(LEFT_BTN)):
                 sleep(0.01)
+            if now_playing.is_playing():
+                now_playing.stop()
             send_string('prev')
-            if not music_number - 1 < 0:
-                music_number = music_number - 1
-            elif not unit_number - 1 < 0:
-                unit_number = unit_number - 1
-                music_number = len(musics[list(musics.keys())[unit_number]]) - 1
-            else:
-                unit_number = len(musics.keys()) - 1
-                music_number = len(musics[list(musics.keys())[unit_number]]) - 1
+            prev_music()
+            sleep(2)
+            now_playing = play_music()
         if not GPIO.input(CENTER_BTN):
             while(not GPIO.input(CENTER_BTN)):
                 sleep(0.01)
+            loop_enable = True
             if now_playing.is_playing():
                 now_playing.stop()
                 send_string('Not playing')
                 continue
             send_string('play music')
             sleep(2)
-            file_name = musics[list(musics.keys())[unit_number]][music_number]
-            file_path = '/home/pi/musics/'+list(musics.keys())[unit_number]+'/'+file_name
-            send_string(remove_num_and_ext(file_name))
-            now_playing = play_file(file_path)
+            now_playing = play_music()
         if not GPIO.input(RIGHT_BTN):
             while(not GPIO.input(RIGHT_BTN)):
                 sleep(0.01)
+            if now_playing.is_playing():
+                now_playing.stop()
             send_string('next')
+            next_music()
+            sleep(2)
+            now_playing = play_music()
 
 finally:
     GPIO.cleanup()
